b8ab82c1e988f2b71e3f14012ad2fb76f3cace77
guillegarri <49150278+guillegarri@users.noreply.github.com>
2019-04-24 11:23:56 +0200

    Add files via upload

--- LinealEquations/main.py
+++ LinealEquations/main.py
@@ -1,31 +1,31 @@
 import sympy as sy
 import numpy as np
 
-from gram_schmidt import qr_gs_decomp, qr_gs_solve, qr_gs_inverse
+from GM import qr_gs_decomp, qr_gs_solve, qr_gs_inverse
 
 n = int(5); # Number of lines
 m = int(4); # Number of columns
 A = np.random.rand(n, m);
-R = np.empty((m, m));
+#R = np.empty((m, m));
 
 print('\nQR DECOMPOSITION\n')
 print('Matrix A 5x4 randomly generated:\n');
 print(A)
 # Decomposition QR
-qr_gs_decomp(A, R);
+(Q, R) = qr_gs_decomp(A);
 
 print('\nMatrix Q 5x4:\n');
-print(A)
+print(Q)
 print('\nMatrix upper triangular R 4x4:\n');
 print(R);
 
 # Check Qt*Q = 1 and Q*R = A
 print('\nCheck that Qt*Q = I:\n')
-QtQ = np.matmul(A.T, A);
+QtQ = np.matmul(Q.T, Q);
 print(QtQ);
 
 print('\nCheck that Q*R = A:\n')
-QR = np.matmul(A, R);
+QR = np.matmul(Q, R);
 print(QR);
 
 # Linear equation solve
@@ -38,36 +38,38 @@ RR = np.empty((ns, ns));
 print('Random matrix A:\n');
 print(AA);
 # Decomposition QR
-qr_gs_decomp(AA, RR);
+(Qb, Rb) = qr_gs_decomp(AA);
 
 # System solving
-qr_gs_solve(AA, RR, b, x);
+x_sol = qr_gs_solve(Qb, Rb, b);
 
 print('\nRandom vector b:\n');
 print(b);
 
 print('\nSolution (x) of the system of linear equations A*x = Q*Rx = b:\n');
-print(x);
+print(x_sol);
 
 # Check that we get b back after A*x
 print('\nCheck that A*x = b:\n');
 b2 = np.empty(ns);
-A2 = np.matmul(AA, RR);
-b2 = np.matmul(A2, x);
+A2 = np.matmul(Qb, Rb);
+b2 = np.matmul(A2, x_sol);
 print(b2);
+
+#print(np.linalg.solve(A2, b));
 
 # Inverse calculation
 print('\nINVERSE CALCULATION\n');
 # We will use the same A matrix from the equation system
 B = np.empty((ns, ns));
-qr_gs_inverse(AA, RR, B);
+B_sol = qr_gs_inverse(AA);
 print('Inverse matrix of A (from the lineal equation system), B:\n');
-print(B);
+print(B_sol);
 
 print('\nCheck that A*B = I:\n');
-I1 = np.matmul(A2, B);
+I1 = np.matmul(AA, B_sol);
 print(I1);
 
 print('\nCheck that B*A = I:\n');
-I2 = np.matmul(B, A2);
+I2 = np.matmul(B_sol, AA);
 print(I2);
